1277394f192cbf869ee5a2d45689ef01a88e1601
Oz <oharrison1998@outlook.com>
2021-07-05 13:19:33 +0100

    Added createTableStructure.js

---
+++ js/createTableStructure.js
@@ -0,0 +1,16 @@
+function createTableStructure(tableTitle) {
+  const table = document.createElement("table");
+  const thead = document.createElement("thead");
+  const tbody = document.createElement("tbody");
+  const theadRow = document.createElement("tr");
+  table.id = tableTitle;
+  thead.id = tableTitle + "Head";
+  tbody.id = tableTitle + "Body";
+  theadRow.id = tableTitle + "HeadRow";
+
+  table.appendChild(thead);
+  table.appendChild(tbody);
+  theadRow.appendChild(theadRow);
+
+  return table;
+}
